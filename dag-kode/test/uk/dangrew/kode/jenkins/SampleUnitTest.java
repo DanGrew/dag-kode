6a9eb2d8b395bc7a49580199e702e4c167e8608a
DanGrew <danielanthonygrew@gmail.com>
2017-07-25 22:41:25 +0100

    SAMPLE TESTS FOR JENKINS:

---
+++ dag-kode/test/uk/dangrew/kode/jenkins/SampleUnitTest.java
@@ -0,0 +1,21 @@
+/*
+ * ----------------------------------------
+ *        Jenkins Test Tracker Model
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ *                 2017
+ * ----------------------------------------
+ */
+package uk.dangrew.kode.jenkins;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleUnitTest {
+
+   @Test public void shouldPerformBasicAssertion() {
+      assertTrue( true );
+   }//End Method
+
+}//End Class
