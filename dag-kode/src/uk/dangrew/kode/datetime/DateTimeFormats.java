d5d7d03b27957b9034148149bf87d5a3b9b3123c
DanGrew <danielanthonygrew@gmail.com>
2018-05-08 08:10:48 +0100

    DATE TIME FORMATS:
    Added supporting methods for timestamp conversions.

--- dag-kode/src/uk/dangrew/kode/datetime/DateTimeFormats.java
+++ dag-kode/src/uk/dangrew/kode/datetime/DateTimeFormats.java
@@ -1,14 +1,63 @@
 package uk.dangrew.kode.datetime;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeFormats {
 
+   public static final String UNKNOWN = "Unknown";
    private static final DateTimeFormatter NAME_NUMBERMONTH_YEAR = DateTimeFormatter.ofPattern( "EEEE dd MMMM yyyy" );
+   private static final DateTimeFormatter TIMESTAMP = DateTimeFormatter.ofPattern( "dd-MM-yyyy HH:mm:ss" );
    
    public String nameNumberMonthYear( LocalDate date ) {
       return NAME_NUMBERMONTH_YEAR.format( date );
+   }//End Method
+   
+   public String toTimestampString( LocalDateTime timestamp ) {
+      if ( timestamp == null ) {
+         return UNKNOWN;
+      }
+      return TIMESTAMP.format( timestamp );
+   }//End Method
+   
+   public String toTimestampString( Number epochSeconds ) {
+      if ( epochSeconds == null ) {
+         return UNKNOWN;
+      }
+      return toTimestampString( LocalDateTime.ofEpochSecond( epochSeconds.longValue(), 0, ZoneOffset.UTC ) );
+   }//End Method
+   
+   public LocalDateTime parseTimestamp( String timestamp ) {
+      if ( timestamp == null ) {
+         return null;
+      }
+      try { 
+         return LocalDateTime.parse( timestamp, TIMESTAMP );
+      } catch ( DateTimeException exception ) {
+         return null;
+      }
+   }//End Method
+   
+   public long parseTimestampToEpochSeconds( String timestamp ) {
+      if ( timestamp == null ) {
+         return 0;
+      }
+      LocalDateTime parsed = parseTimestamp( timestamp );
+      if ( parsed == null ) {
+         return 0;
+      }
+      return parsed.toEpochSecond( ZoneOffset.UTC );
+   }//End Method
+   
+   public long toDayBeginningEpochSeconds( LocalDate timestamp ) {
+      if ( timestamp == null ) {
+         return 0L;
+      }
+      return LocalDateTime.of( timestamp, LocalTime.MIN ).toEpochSecond( ZoneOffset.UTC );
    }//End Method
    
 }//End Class
--- dag-kode/test/uk/dangrew/kode/datetime/DateTimeFormatsTest.java
+++ dag-kode/test/uk/dangrew/kode/datetime/DateTimeFormatsTest.java
@@ -1,15 +1,21 @@
 package uk.dangrew.kode.datetime;
 
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class DateTimeFormatsTest {
 
+   private static final LocalDateTime SUBJECT = LocalDateTime.of( 2018, 5, 8, 8, 3 );
+   
    private DateTimeFormats systemUnderTest;
 
    @Before public void initialiseSystemUnderTest() {
@@ -19,6 +25,31 @@ public class DateTimeFormatsTest {
    @Test public void shouldFormatWithReadableDay() {
       assertThat( systemUnderTest.nameNumberMonthYear( LocalDate.of( 2018, 1, 23 ) ), is( "Tuesday 23 January 2018" ) );
       assertThat( systemUnderTest.nameNumberMonthYear( LocalDate.of( 2018, 1, 3 ) ), is( "Wednesday 03 January 2018" ) );
+   }//End Method
+   
+   @Test public void shouldFormatTimestamp(){
+      assertThat( systemUnderTest.toTimestampString( SUBJECT ), is( "08-05-2018 08:03:00" ) );
+      assertThat( systemUnderTest.toTimestampString( ( LocalDateTime )null ), is( DateTimeFormats.UNKNOWN ) );
+   }//End Method
+   
+   @Test public void shouldFormatTimestampSeconds(){
+      assertThat( systemUnderTest.toTimestampString( SUBJECT.toEpochSecond( ZoneOffset.UTC ) ), is( "08-05-2018 08:03:00" ) );
+      assertThat( systemUnderTest.toTimestampString( ( Number )null ), is( DateTimeFormats.UNKNOWN ) );
+   }//End Method
+   
+   @Test public void shouldParseTimestamp(){
+      assertThat( systemUnderTest.parseTimestamp( systemUnderTest.toTimestampString( SUBJECT ) ), is( SUBJECT ) );
+      assertThat( systemUnderTest.parseTimestamp( null ), is( nullValue() ) );
+   }//End Method
+   
+   @Test public void shouldParseTimestampToEpochSeconds(){
+      assertThat( systemUnderTest.parseTimestampToEpochSeconds( systemUnderTest.toTimestampString( SUBJECT ) ), is( SUBJECT.toEpochSecond( ZoneOffset.UTC ) ) );
+      assertThat( systemUnderTest.parseTimestampToEpochSeconds( null ), is( 0L ) );
+   }//End Method
+   
+   @Test public void shouldFormatToDayBeginning(){
+      assertThat( systemUnderTest.toDayBeginningEpochSeconds( SUBJECT.toLocalDate() ), is( 1525737600L ) );
+      assertThat( systemUnderTest.toDayBeginningEpochSeconds( null ), is( 0L ) );
    }//End Method
 
 }//End Class
