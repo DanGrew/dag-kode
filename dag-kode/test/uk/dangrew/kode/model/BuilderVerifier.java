1fe96e2b336fa65f3b5ae2dfa3d0ebdbd973ee5e
DanGrew <danielanthonygrew@gmail.com>
2018-05-26 10:04:21 +0100

    BUILD VERIFIER UPDATE:

--- dag-kode/test/uk/dangrew/kode/model/BuilderVerifier.java
+++ dag-kode/test/uk/dangrew/kode/model/BuilderVerifier.java
@@ -3,6 +3,7 @@ package uk.dangrew.kode.model;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -15,6 +16,26 @@ public class BuilderVerifier< SutT > {
    ) {
       setter.accept( sample );
       assertThat( getter.get(), is( sample ) );
+      return this;
+   }//End Method
+   
+   public BuilderVerifier< SutT > build( 
+            Runnable setter, 
+            Supplier< Boolean > getter, 
+            boolean expected
+   ) {
+      setter.run();
+      assertThat( getter.get(), is( expected ) );
+      return this;
+   }//End Method
+   
+   public < VariableT > BuilderVerifier< SutT > buildWithOptional( 
+            Consumer< VariableT > setter, 
+            Supplier< Optional< VariableT > > getter, 
+            VariableT sample 
+   ) {
+      setter.accept( sample );
+      assertThat( getter.get().get(), is( sample ) );
       return this;
    }//End Method
    
