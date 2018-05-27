69381799eb6bc5f325ed6cb7d74eb42b0cdb1764
DanGrew <danielanthonygrew@gmail.com>
2018-05-27 10:20:49 +0100

    JAVAFX HACKS FOR TABLES:
    Some fiddly ways to look up nodes in a table and this class wraps that
    to make it easier elsewhere.

--- dag-kode/src/uk/dangrew/kode/javafx/hacks/JavaFxHacks.java
+++ dag-kode/src/uk/dangrew/kode/javafx/hacks/JavaFxHacks.java
@@ -1,22 +1,47 @@
 package uk.dangrew.kode.javafx.hacks;
 
 import java.util.ArrayList;
-import java.util.Optional;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 import javafx.scene.Node;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 
 public class JavaFxHacks {
 
    static final String LOOKUP_TABLE_ROW_CELL = ".table-row-cell";
 
-   public Optional< Node > lookupTableRow( TableView< ? > table, int row ) {
-      Set< Node > tableRowCell = table.lookupAll( LOOKUP_TABLE_ROW_CELL );
-      if ( tableRowCell.isEmpty() || row >= tableRowCell.size() ) {
-         return Optional.empty();
+   public < RowObjectT > List< TableRow< RowObjectT > > lookupTableRows( TableView< RowObjectT > table ) {
+      Set< Node > tableRows = table.lookupAll( LOOKUP_TABLE_ROW_CELL );
+      if ( tableRows.isEmpty() ) {
+         return Collections.emptyList();
       }
-      return Optional.of( new ArrayList<>( tableRowCell ).get( row ) );
+      if ( tableRows.iterator().next() instanceof TableRow ) {
+         
+         List< TableRow< RowObjectT > > converted = new ArrayList<>( table.getItems().size() );
+         for ( Node node : tableRows ) {
+            
+            @SuppressWarnings("unchecked")
+            TableRow< RowObjectT > row = ( TableRow< RowObjectT > ) node;
+            if ( row.getItem() == null ) {
+               continue;
+            } else if ( !table.getItems().contains( row.getItem() ) ) {
+               continue;
+            }
+            
+            int index = table.getItems().indexOf( row.getItem() );
+            if ( index >= converted.size() ) {
+               converted.add( row );
+            } else {
+               converted.add( index, row );
+            }
+         }
+         
+         return converted;
+      }
+      return Collections.emptyList();
    }//End Method
    
 }//End Class
--- dag-kode/test/uk/dangrew/kode/javafx/hacks/JavaFxHacksTest.java
+++ dag-kode/test/uk/dangrew/kode/javafx/hacks/JavaFxHacksTest.java
@@ -1,7 +1,8 @@
 package uk.dangrew.kode.javafx.hacks;
 
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -10,17 +11,18 @@ import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import javafx.scene.Node;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import uk.dangrew.kode.launch.TestApplication;
 
 public class JavaFxHacksTest {
 
-   @Mock private TableView< Object > view;
+   @Spy private TableView< String > view;
    private Set< Node > returned;
    private JavaFxHacks systemUnderTest;
 
@@ -30,15 +32,46 @@ public class JavaFxHacksTest {
 
       returned = new LinkedHashSet<>();
       systemUnderTest = new JavaFxHacks();
+      when( view.lookupAll( JavaFxHacks.LOOKUP_TABLE_ROW_CELL ) ).thenReturn( returned );
    }//End Method
 
    @Test public void shouldLookupTableRow() {
-      when( view.lookupAll( JavaFxHacks.LOOKUP_TABLE_ROW_CELL ) ).thenReturn( returned );
-      assertThat( systemUnderTest.lookupTableRow( view, 0 ).isPresent(), is( false ) );
+      assertThat( systemUnderTest.lookupTableRows( view ), is( empty() ) );
       
+      TableRow< String > row1 = new TableRow<>();
+      row1.setItem( new Object().toString() );
+      
+      TableRow< String > row2 = new TableRow<>();
+
+      TableRow< String > row3 = new TableRow<>();
+      row3.setItem( new Object().toString() );
+      
+      TableRow< String > row4 = new TableRow<>();
+      row4.setItem( new Object().toString() );
+      
+      returned.add( row1 );
+      returned.add( row2 );
+      returned.add( row3 );
+      returned.add( row4 );
+      view.getItems().add( row3.getItem() );
+      view.getItems().add( row1.getItem() );
+      view.getItems().add( row4.getItem() );
+      
+      assertThat( systemUnderTest.lookupTableRows( view ), contains( row3, row1, row4 ) );
+   }//End Method
+   
+   @Test public void shouldIgnoreIncompatible() {
+      assertThat( systemUnderTest.lookupTableRows( view ), is( empty() ) );
+      
+      TableRow< Object > row1 = new TableRow<>();
+      row1.setItem( new Object() );
+      
+      returned.add( row1 );
+      assertThat( systemUnderTest.lookupTableRows( view ), is( empty() ) );
+      
+      returned.clear();
       returned.add( new BorderPane() );
-      assertThat( systemUnderTest.lookupTableRow( view, 0 ).get(), is( notNullValue() ) );
-      assertThat( systemUnderTest.lookupTableRow( view, 1 ).isPresent(), is( false ) );
+      assertThat( systemUnderTest.lookupTableRows( view ), is( empty() ) );
    }//End Method
 
 }//End Class
