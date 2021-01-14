0295964c8c417246fd041808a8bb34e9c976be4c
DanGrew <danielanthonygrew@gmail.com>
2021-01-14 10:33:11 +0000

    HEADER UPDATE: Fixed silly mistake in ide configuration.

--- dag-kode/test/uk/dangrew/kode/javafx/controls/DirectionControlTypeTest.java
+++ dag-kode/test/uk/dangrew/kode/javafx/controls/DirectionControlTypeTest.java
@@ -1,15 +1,4 @@
-// *************************************************************
-//                         H*E*R*M*E*S
-//                   Holistic Environment for
-//         Railway Modelling, Evaluation and Simulation
-//        Built on Graffica System Development Kit: GSDK
-//
-//       Copyright: (c) Graffica Ltd (www.graffica.co.uk)
-//
-// This software is made available under binary licence. Holding 
-// source code without the express permission of Graffica Ltd is 
-//           not permitted under any circumstances. 
-// *************************************************************
+
 
 package uk.dangrew.kode.javafx.controls;
 
