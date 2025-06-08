ca30e11d5e5c5465effd3be1d472bb2e249416b3
Dan <danielanthonygrew@gmail.com>
2025-06-08 12:41:09 +0100

    This or that formatter latest

---
+++ dag-kode/test/dump/words/ThisOrThatFormatter.java
@@ -0,0 +1,82 @@
+package dump.words;
+
+import java.util.*;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ThisOrThatFormatter {
+    private final Random random;
+
+    private final Map<String, String> matches;
+
+    public ThisOrThatFormatter(Random random) {
+        this.random = random;
+        this.matches = new HashMap<>();
+    }
+
+    public void matchWords(List<String> gridWords, List<String> allWords) {
+        final var matchList = new ArrayList<>(allWords);
+        matchList.removeAll(gridWords);
+        gridWords.sort(Comparator.comparingInt(String::length).reversed());
+
+        tryMatch(gridWords, matchList, (s, l) -> s.length() == l);
+        tryMatch(gridWords, matchList, (s, l) -> l - s.length() == 1);
+        tryMatch(gridWords, matchList, (s, l) -> l - s.length() == 2);
+        tryMatch(gridWords, matchList, (s, l) -> s.length() - l == 1);
+        tryMatch(gridWords, matchList, (s, l) -> s.length() - l == 2);
+        tryMatch(gridWords, matchList, (s, l) -> s.length() >= l);
+        tryMatch(gridWords, matchList, (s, l) -> s.length() < l);
+        for (String gridWord : gridWords) {
+            System.out.printf("Can't match: [%s]\n", gridWord);
+        }
+    }
+
+    private void tryMatch(List<String> gridWords, List<String> matchList, BiPredicate<String, Integer> filter) {
+        final var matched = new ArrayList<>();
+        for (String gridWord : gridWords) {
+            if (gridWord.equalsIgnoreCase("quoits")){
+                System.out.println();
+            }
+            int length = gridWord.length();
+            final var match = matchList.stream()
+                    .filter(s -> filter.test(s, length))
+                    .sorted(Comparator.comparing(String::length))
+                    .findFirst();
+            if (match.isPresent()) {
+                handleMatch(gridWord, match.get(), matchList);
+                matched.add(gridWord);
+            }
+        }
+        gridWords.removeAll(matched);
+    }
+
+    private void handleMatch(String gridWord, String match, List<String> matchList) {
+        if (match.isEmpty()) {
+            System.out.printf("Can't match: [%s]\n", gridWord);
+        } else {
+            matches.put(gridWord, match);
+            matchList.remove(match);
+        }
+    }
+
+    public String getMatch(String gridWord) {
+        return matches.getOrDefault(gridWord, null);
+    }
+
+    public List<String> buildWordList() {
+        return matches.entrySet().stream()//.sorted(Comparator.comparing(e -> e.getKey(), String::compareToIgnoreCase))
+                .map(e -> format(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    private String format(String gridWord, String match) {
+        final var gridWordFirst = random.nextBoolean();
+        if (gridWordFirst) {
+            return gridWord + " OR " + match;// + String.format("[gw: %d, m: %d]", gridWord.length(), match.length());
+        } else {
+            return match + " OR " + gridWord;// + String.format("[gw: %d, m: %d]", gridWord.length(), match.length());
+        }
+    }
+}
---
+++ dag-kode/test/dump/words/ThisOrThatFormatterTest.java
@@ -0,0 +1,139 @@
+package dump.words;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ThisOrThatFormatterTest {
+
+    private Random random;
+    private ThisOrThatFormatter sut;
+
+    @Before
+    public void initSut() {
+        random = mock(Random.class);
+        sut = new ThisOrThatFormatter(random);
+    }
+
+    @Test
+    public void shouldFindMatch() {
+        sut.matchWords(List.of("start"), List.of("start", "match"));
+        assertEquals("match", sut.getMatch("start"));
+    }
+
+    @Test
+    public void shouldFindMatch_sameLength() {
+        sut.matchWords(List.of("start"), List.of("start", "4444", "55555", "666666"));
+        assertEquals("55555", sut.getMatch("start"));
+    }
+
+    @Test
+    public void shouldFindMatch_nextLongestLength() {
+        sut.matchWords(List.of("start"), List.of("start", "4444", "666666"));
+        assertEquals("666666", sut.getMatch("start"));
+    }
+
+    @Test
+    public void shouldFindMatch_anythingAvailable() {
+        sut.matchWords(List.of("start"), List.of("start", "88888888"));
+        assertEquals("88888888", sut.getMatch("start"));
+    }
+
+    @Test
+    public void shouldFormatOutput() {
+        sut.matchWords(List.of("start"), List.of("start", "match"));
+
+        when(random.nextBoolean()).thenReturn(true).thenReturn(false);
+        assertEquals("start OR match", sut.buildWordList().get(0));
+        assertEquals("match OR start", sut.buildWordList().get(0));
+    }
+
+    @Test
+    public void shouldNotReuseWords(){
+
+    }
+
+    @Test
+    public void shouldFormatOutput_realExample() {
+        sut.matchWords(new ArrayList<>(List.of(
+                "Ball",
+                "Bike",
+                "Binoculars",
+                "Car",
+                "Card",
+                "Chalk",
+                "Clown",
+                "Crib",
+                "Dollhouse",
+                "Domino",
+                "Drum",
+                "Frisbee",
+                "Helmet",
+                "Hulahoop",
+                "Jigsaw",
+                "Jumprope",
+                "Kaleidoscope",
+                "Kite",
+                "Maraca",
+                "Marker",
+                "Mask",
+                "Nerfgun",
+                "Plane",
+                "Puzzle",
+                "Quoits",
+                "Sandbox",
+                "Scooter",
+                "Skateboard",
+                "Teddy",
+                "Train",
+                "Xylophone",
+                "Zipper")), List.of(
+                "Boat",
+                "Lego",
+                "Inflatable",
+                "Cube",
+                "Top",
+                "Globe",
+                "Tower",
+                "Doll",
+                "Slingshot",
+                "Kickball",
+                "Dice",
+                "Puppet",
+                "Unicycle",
+                "Chalkboard",
+                "Lollipop",
+                "Velcroball",
+                "Balancebeam",
+                "Sled",
+                "Rocket",
+                "Crayon",
+                "Yoyo",
+                "Balloon",
+                "Easel",
+                "Bubble",
+                "Swing",
+                "Playdough",
+                "Origami",
+                "Fingerpuppet",
+                "Robot",
+                "Truck",
+                "Nestingdoll",
+                "Marbles"
+        ));
+
+        when(random.nextBoolean()).thenReturn(true);
+
+        sut.buildWordList().forEach(w -> System.out.println(w));
+    }
+
+
+}
